7512f8c53226414d1d6e0476005b57d205b8f67a
Leidens <63185910+Leidens@users.noreply.github.com>
2021-03-13 12:12:05 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const bot = new Discord.Client()
 
 const Command = require('./command/command')
 const Rib = require('./command/rib')
-const PORT = process.env.PORT || 3000;
 
 
 bot.on('ready', function() {
     bot.user.setActivity("Je sais qu'est ce que je dit")
-  })
-
-const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-      console.log(`Votre app est lancé sur le port ${ PORT }`);
   })
 
 bot.on('message', message => {

2dce4ff78daf2b3337f1926c700d3e192e8ccf0e
Leidens <63185910+Leidens@users.noreply.github.com>
2021-03-13 11:14:33 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    TOKEN: "ODIwMjI1ODQ0OTY2MDY0MTYw.YEyE2A.T1rl7baRpS9ydiUDUYB0oexgKII",
+    TOKEN: "ODIwMjI1ODQ0OTY2MDY0MTYw.YEyE2A.igVn6NuDrcj4goabmHC0E3nM7ss",
     }
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bot = new Discord.Client()
 const Command = require('./command/command')
 const Rib = require('./command/rib')
 
-bot.login (process.env.TOKEN)
 
 bot.on('ready', function() {
     bot.user.setActivity("Je sais qu'est ce que je dit")
@@ -13,4 +12,6 @@ bot.on('ready', function() {
 bot.on('message', message => {
     Rib.parse(message)
 })
+
+client.login(process.env.TOKEN);
 
